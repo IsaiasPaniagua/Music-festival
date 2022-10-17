2b889139c41929361363c412a79c0e178ca7641f
Isaias Paniagua <isaias.paniagua19@tectijuana.edu.mx>
2022-10-16 22:54:11 -0700

    add function javascript

--- gulpfile.js
+++ gulpfile.js
@@ -34,16 +34,38 @@ const {src, dest, watch, parallel} = require('gulp');
 
 // CSS
 const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber')
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer'); // Va asegurarse de que los estilos funcionen en cualquier navegador
+const cssnano = require('cssnano'); // Va a comprimir nuestro codigo de css
+const postcss = require('gulp-postcss'); // Va a hacer algunas transformaciones por medio de los dos de arriba
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes 
-const webp = require('gulp-webp')
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+
+// JS
+const terser = require('gulp-terser-js');
 
 function css(done) {
     src('src/scss/**/*.scss') //identificar el archivo sass a compilar
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass()) //compilarlo
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));      //almacenarla
+    done();
+}
+
+function imagenes(done){
+    const opciones = {
+        optimizationLevel:3
+    }
+    src('src/img/**/*.{png, jpg}')
+    .pipe( cache( imagemin(opciones)))
+    .pipe( dest('build.img'))
     done();
 }
 
@@ -59,13 +81,26 @@ function versionWebp(done) {
     done();
 }
 
+function javascript( done) {
+    src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
+}
+
 function dev(done) {
 
     watch('src/scss/**/*.scss', css);
+    watch('src/js/**/*.js', javascript);
 
     done();
 }
 
 exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.dev = parallel(imagenes, versionWebp,javascript, dev);
